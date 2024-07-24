693d0783713c528f193029d368246799cd924ca6
APATAKER <therealthelip@gmail.com>
2024-07-25 00:17:16 +0530

    Added files for Task 4

---
+++ WD/script2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <file>"
+    exit 1
+fi
+file=$1
+temp_file=$(mktemp)
+awk 'NR < 5 { print; next } /welcome/ { gsub(/give/, "learning") } { print }' "$file" > "$temp_file"
+mv "$temp_file" "$file"
+echo "File processing complete. All occurrences of 'give' have been replaced with 'learning' from line 5 onwards in lines containing 'welcome'."
