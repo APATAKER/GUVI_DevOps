55c057e6c7420154ea9e15ace62ce9066d041f1a
APATAKER <therealthelip@gmail.com>
2024-08-19 11:04:34 -0400

    Added a readme file for docker Task 2

--- 7. Docker Task 2/WebApp/app/app.py
+++ 7. Docker Task 2/WebApp/app/app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return '''
-    <h1>About Me</h1>
-    <p>Name: ChatGPT</p>
-    <p>Version: GPT-4</p>
-    <p>Specialty: Language Model AI</p>
+    <h1>Hello and Welcome to DevOps</h1>
+    <p>Name: Raja</p>
+    <p>Version: M</p>
+    <p>Specialty: new learner</p>
     '''
 
 if __name__ == '__main__':
---
+++ 7. Docker Task 2/readme.txt
@@ -0,0 +1,13 @@
+To Run it yourself
+
+- have docker installed
+
+From the WebApp directory run the following commands 
+
+    docker-compose build
+
+
+    docker-compose up
+
+    
+    docker-compose down
