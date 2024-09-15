002018652461c2bd950e48e93d920b32b0c56108
APATAKER <therealthelip@gmail.com>
2024-09-15 11:43:52 -0400

    another test in changing app.js

--- app.js
+++ app.js
@@ -4,13 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World This is Raja Welcome to Devops');
 });
 
 app.listen(port, () => {
