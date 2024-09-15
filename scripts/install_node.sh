1c0c554877886d855d0249178f943b51a1da27a1
APATAKER <therealthelip@gmail.com>
2024-09-15 11:51:16 -0400

    Adding script for clean build

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -2,3 +2,6 @@
 echo "Installing Node.js and npm..."
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
 sudo yum install -y nodejs
+cd /var/www/html
+echo "Removing node_modules directory..."
+rm -rf node_modules
