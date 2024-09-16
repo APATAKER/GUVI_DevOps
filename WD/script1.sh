0bd78a36721633d88b721703a4acabc99b633d35
APATAKER <therealthelip@gmail.com>
2024-09-16 01:08:00 -0400

    Cleaning up maven branch

--- WD/script1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-http_code=$(curl -o /dev/null -s -w "%{http_code}\n" https://www.guvi.in)
-echo "HTTP status code: $http_code"
-if [ "$http_code" -eq 200 ]; then
-    echo "Success: The website is reachable."
-else
-    echo "Failure: The website returned an error code."
-fi
-
